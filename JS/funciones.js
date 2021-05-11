816b85bb521fe3186210cb58252a462b6520ec63
EtuQmiras <74884509+EtuQmiras@users.noreply.github.com>
2021-05-11 12:26:00 +0200

    Add files via upload

---
+++ JS/funciones.js
@@ -0,0 +1,46 @@
+let slider = document.querySelector(".slider-contenedor")
+let sliderIndividual = document.querySelectorAll(".contenido-slider")
+let contador = 1;
+let width = sliderIndividual[0].clientWidth;
+let intervalo = 3000;
+
+window.addEventListener("resize", function(){
+    width = sliderIndividual[0].clientWidth;
+})
+
+setInterval(function(){
+    slides();
+},intervalo);
+
+
+
+function slides(){
+    slider.style.transform = "translate("+(-width*contador)+"px)";
+    slider.style.transition = "transform .8s";
+    contador++;
+
+    if(contador == sliderIndividual.length){
+        setTimeout(function(){
+            slider.style.transform = "translate(0px)";
+            slider.style.transition = "transform 0s";
+            contador=1;
+        },1500)
+    }
+}
+
+function checknext() {
+  if (document.querySelector(".last:checked")) {
+    document.querySelector(".first").checked = true;
+  } else {
+    document.querySelector(".nav:checked").nextElementSibling.checked = true;
+  }
+}
+
+setInterval(function () {
+  checknext();
+}, 3000);
+
+
+
+
+
